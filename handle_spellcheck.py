1ac32be102e711734e23f7d339ff85eacafc14dc
kagrawal7 <kush.agr02@gmail.com>
2024-05-31 14:52:56 -0400

    fully implemented sentence spellcheck as well. added a function in handle_spellcheck to handle everything and act as main

--- handle_spellcheck.py
+++ handle_spellcheck.py
@@ -1,6 +1,20 @@
+from transformers import pipeline
+from span_marker import SpanMarkerModel
 import string
 import re
 from nltk_spellchecking import name_spellcheck
+
+
+def load_models() -> list:
+    """ load the model that checks for names
+    and the english spelling pipeline
+    """
+    ner_model = SpanMarkerModel.from_pretrained(
+        "tomaarsen/span-marker-roberta-large-ontonotes5")
+    ner_model.cuda()
+    eng_spell_pipeline = pipeline(
+        "text2text-generation", model="oliverguhr/spelling-correction-english-base")
+    return ner_model, eng_spell_pipeline
 
 
 def punctuation_spacing(prompt: str) -> str:
@@ -21,21 +35,14 @@ def punctuation_spacing(prompt: str) -> str:
     return ans
 
 
-def give_suggestion(original: str, list_of_dicts: list, include_transpositions=False) -> str:
+def correct_name(original: str, list_of_dicts: list, include_transpositions=False) -> str:
     """
     Take in the list of dictionaries given by model
     spell check name, whole sentence
     return suggestion
     """
-    # example = [{'span': 'John', 'label': 'PERSON', 'score': 0.9854856729507446, 'char_start_index': 6, 'char_end_index': 10},
-    #            {'span': '15', 'label': 'DATE', 'score': 0.9371686577796936, 'char_start_index': 22, 'char_end_index': 24}]
 
-    # # ================ SOLUTION 1 ========================
-    # # extract name strings, call the spell check on them,
-    # # then call the spell check on the whole string with the corrected names
-    # #  - not sure if the names will interfere when checking rest of sentence?
-
-    correct_name_prompt = original
+    correct_prompt = original
     original_names = []
 
     for dict in list_of_dicts:
@@ -47,18 +54,34 @@ def give_suggestion(original: str, list_of_dicts: list, include_transpositions=F
             # corrected_name = func(span_name)
             corrected_name = name_spellcheck.edit_dist_suggestion(
                 span_name, include_transpositions)
-            correct_name_prompt = re.sub(" {old_name} ".format(
-                old_name=span_name), " {new_name} ".format(new_name=corrected_name), correct_name_prompt)
+            correct_prompt = re.sub(" {old_name} ".format(
+                old_name=span_name), " {new_name} ".format(new_name=corrected_name), correct_prompt)
+    return correct_prompt
 
-    
+
+# ================ MAIN FUNCTION ========================
+def handle_spellcheck(input: str, transpositions=False):
+    """
+    separate punctuation from letters, 
+    load ner model and run inference, load pipeline
+    get corrected name prompt,
+    run spellcheck on full sentence
+    return suggestion
+    """
+    prompt = punctuation_spacing(input)
+    ner_model, spelling_pipeline = load_models()
+
+    # # ================ SOLUTION 1 ========================
+    # # extract name strings, call the spell check on them,
+    # # then call the spell check on the whole string with the corrected names
+    # #  - not sure if the names will interfere when checking rest of sentence?
+    correct_name_prompt = correct_name(
+        prompt, ner_model.predict(prompt), transpositions)
+    suggestion = spelling_pipeline(correct_name_prompt, max_length=2048)
+    return suggestion
+
     # # ================ SOLUTION 2 ========================
     # # extract name strings, call the spell check on them,
     # # call spell check on the remaining sentence
     # # with placeholder values for the name.
-
     # # If time, implement multithreading?
-
-    # # spell check rest of sentence?
-
-    suggestion = correct_name_prompt
-    return suggestion
--- test_handle_spellcheck.py
+++ test_handle_spellcheck.py
@@ -1,4 +1,5 @@
 from span_marker import SpanMarkerModel
+from transformers import pipeline
 from handle_spellcheck import punctuation_spacing, give_suggestion
 import add_noise
 import string
@@ -11,14 +12,17 @@ if __name__ == "__main__":
         "tomaarsen/span-marker-roberta-large-ontonotes5")
     ner_model.cuda()
 
+    eng_spell_pipeline = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base")
+
     # for char in string.punctuation:
     #     input = "I went to the park with my friend Kushagra" + char + " Vagisha joined us later" + char
     #     prompt = punctuation_spacing(input)
     name = "J. V. Ramana"
     input = add_noise.noisy(" How many votes did ", 0.15) + \
         add_noise.noisy(name, 0) + add_noise.noisy(",. have? ", 0.15)
+    print("Input: {x}".format(x=input))
     prompt = punctuation_spacing(input)
-    output = give_suggestion(prompt, ner_model.predict(prompt))
+    output = eng_spell_pipeline(give_suggestion(prompt, ner_model.predict(prompt)), max_length=2048)
 
     print("Input: {x}".format(x=input))
     print("Output: {y}\n".format(y=output))
