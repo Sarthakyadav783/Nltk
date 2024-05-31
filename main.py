407644369e227e2b1b1003c7e5a65353754df4b1
kagrawal7 <kush.agr02@gmail.com>
2024-05-31 14:53:18 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
-from span_marker import SpanMarkerModel
-from handle_spellcheck import punctuation_spacing, give_suggestion
+from handle_spellcheck import handle_spellcheck
 
 
 app = FastAPI()
-# note: Are we guaranteed input will be in english? not sure how the model
-# would respond to input in another language
-ner_model = SpanMarkerModel.from_pretrained(
-    "tomaarsen/span-marker-roberta-large-ontonotes5")
-ner_model.cuda()
-
 
 @app.get("/")
 def string_correct(input: str):
-
+    # note: Are we guaranteed input will be in english? not sure how the model
+    # would respond to input in another language
     # Model struggles when name surrounded by punctuation, so:
-    prompt = punctuation_spacing(input)
-    suggestion = give_suggestion(prompt, ner_model.predict(prompt),
-                                 include_transpositions=True)
+    suggestion = handle_spellcheck(input, transpositions=True)
     return suggestion
