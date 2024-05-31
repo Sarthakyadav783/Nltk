8e722e4dac529db738e65caa85b377a3b1c4c678
kagrawal7 <kush.agr02@gmail.com>
2024-05-31 18:43:08 -0400

    added helper file for handler, moved some functions over

--- handler.py
+++ handler.py
@@ -3,9 +3,9 @@ from span_marker import SpanMarkerModel
 import warnings
 import string
 import re
-from nltk import edit_distance
-import name_spellcheck
-import nltk_correct_txt
+from handler_helper import final_check, punctuation_spacing
+from name_spellcheck import edit_dist_suggestion
+from nltk_correct_txt import did_you_mean
 
 
 def load_models() -> list:
@@ -18,55 +18,6 @@ def load_models() -> list:
     eng_spell_pipeline = pipeline(
         "text2text-generation", model="oliverguhr/spelling-correction-english-base")
     return ner_model, eng_spell_pipeline
-
-
-def final_check(prompt: str, suggestion: str) -> str:
-    """
-    the spell checking model seems to add a few unnecessary tokens
-    at the end that are completely false. add a check for this
-    and return the real answer.
-
-    currently kinda slow; should think about faster solution?
-    """
-    prompt_tokens = prompt.split(" ")
-    last_word = ""
-    for i in range(len(prompt_tokens) - 1, -1, -1):
-        prompt_token = prompt_tokens[i]
-        if prompt_token not in string.punctuation and prompt_token not in string.whitespace:
-            last_word += prompt_token
-            break
-
-    suggestion_tokens = suggestion.split(" ")
-    n = len(suggestion_tokens)
-
-    # loop from the end and go backwards
-    # keep in mind that the spellcheck suggestion adds proper punctuation
-    for i in range(n-1, -1, -1):
-        # go backwards; see if token in prompt (some similarity threshold)
-        # once you find the first token that meets the threshold
-        # and is not punctuation, break the loop
-        token = suggestion_tokens[i]
-        if token not in string.punctuation and edit_distance(token, last_word) < 4:
-            return " ".join(suggestion_tokens[: i+1])
-    return ""
-
-
-def punctuation_spacing(prompt: str) -> str:
-    """
-    Add space around punctuation
-    """
-    special_regex_chars = [".", "+", "*", "?", "^",
-                           "$", "(", ")", "[", "]", "{", "}", "|", "\\"]
-    already_seen = []
-    ans = prompt
-    for char in prompt:
-        if char not in already_seen:
-            already_seen.append(char)
-            if char in special_regex_chars:
-                ans = re.sub("\{}".format(char), " {} ".format(char), ans)
-            elif char in string.punctuation:
-                ans = re.sub(char, " {} ".format(char), ans)
-    return ans
 
 
 def correct_name(original: str, list_of_dicts: list, include_transpositions=False) -> str:
@@ -116,7 +67,7 @@ def handle_spellcheck(input: str, transpositions=False) -> list:
     pipeline_suggestion = final_check(
         prompt, pipeline_output[0]['generated_text'])
 
-    nltk_suggestion = nltk_correct_txt.did_you_mean(correct_name_prompt)
+    nltk_suggestion = did_you_mean(correct_name_prompt)
 
     return [pipeline_suggestion, nltk_suggestion]
 
---
+++ handler_helper.py
@@ -0,0 +1,52 @@
+import string
+import re
+from nltk.metrics import edit_distance
+
+
+def final_check(prompt: str, suggestion: str) -> str:
+    """
+    the spell checking model seems to add a few unnecessary tokens
+    at the end that are completely false. add a check for this
+    and return the real answer.
+
+    currently kinda slow; should think about faster solution?
+    """
+    prompt_tokens = prompt.split(" ")
+    last_word = ""
+    for i in range(len(prompt_tokens) - 1, -1, -1):
+        prompt_token = prompt_tokens[i]
+        if prompt_token not in string.punctuation and prompt_token not in string.whitespace:
+            last_word += prompt_token
+            break
+
+    suggestion_tokens = suggestion.split(" ")
+    n = len(suggestion_tokens)
+
+    # loop from the end and go backwards
+    # keep in mind that the spellcheck suggestion adds proper punctuation
+    for i in range(n-1, -1, -1):
+        # go backwards; see if token in prompt (some similarity threshold)
+        # once you find the first token that meets the threshold
+        # and is not punctuation, break the loop
+        token = suggestion_tokens[i]
+        if token not in string.punctuation and edit_distance(token, last_word) < 4:
+            return " ".join(suggestion_tokens[: i+1])
+    return ""
+
+
+def punctuation_spacing(prompt: str) -> str:
+    """
+    Add space around punctuation
+    """
+    special_regex_chars = [".", "+", "*", "?", "^",
+                           "$", "(", ")", "[", "]", "{", "}", "|", "\\"]
+    already_seen = []
+    ans = prompt
+    for char in prompt:
+        if char not in already_seen:
+            already_seen.append(char)
+            if char in special_regex_chars:
+                ans = re.sub("\{}".format(char), " {} ".format(char), ans)
+            elif char in string.punctuation:
+                ans = re.sub(char, " {} ".format(char), ans)
+    return ans
