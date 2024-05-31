3a9da001eb076fcedb4a3e84d79037f563628e0d
kagrawal7 <kush.agr02@gmail.com>
2024-05-31 01:47:16 -0400

    added name spellcheck testing

--- test_prompts.py
+++ test_prompts.py
@@ -2,8 +2,9 @@
 from nltk.corpus import words
 import random
 import pandas as pd
-from nltk_spellchecking import nltk_correct_txt, nltk_spell_check
-import nltk_spellchecking.correction as correction
+from nltk_spellchecking import nltk_correct_txt, nltk_spell_check, correction
+
+from nltk_spellchecking import name_spellcheck
 import add_noise
 
 
@@ -15,8 +16,7 @@ NOISE_STRENGTH = 0.13
 
 def english_outputs(input: str):
     """
-    Print the input prompt, and the output from the various 
-    corrector functions in this module
+    Print the input prompt (only general English), and the output from various spellchecking modules 
     """
     print(f"\nInput text: {input}")
     print(
@@ -27,22 +27,34 @@ def english_outputs(input: str):
         f"Output from nltk_spell_check.spell_check(): {nltk_spell_check.spell_check(input)}")
 
 
+def name_outputs(input: str):
+    """
+    Print the input prompt (only names), and the output from various spellchecking modules 
+    """
+    print(f"\nInput text: {input}")
+    print(f"Output from name_spellcheck.edit_dist_suggestion(): {name_spellcheck.edit_dist_suggestion(input)}")
+    
+
+
 def whole_sentence_outputs(input: str):
     """
-    Print the input prompt, and the output from the various 
-    corrector functions in this module
+    Print the input prompt, and the output from various spellchecking modules
     """
     print(f"\nInput text: {input}")
     print(
         f"Output from correction.correct_text(): {correction.correct_text(input, df)}")
 
 
-def run_test(file_name: str, test_type: str):
+def run_test(file_name: str, test_type: str, limit = -1):
     """
     file must be in same directory
     """
+    i = 0
     with open(file_name, "r") as file:
         for prompt in file:
+            if i == limit:
+                break
+            i += 1
             input = ""
             if ADD_NOISE:
                 input += add_noise.noisy(prompt, NOISE_STRENGTH)
@@ -52,8 +64,7 @@ def run_test(file_name: str, test_type: str):
             if test_type == "just english":
                 english_outputs(input)
             elif test_type == "just name":
-                # not implemented yet
-                return
+                name_outputs(input)
             elif test_type == "everything":
                 whole_sentence_outputs(input)
             else:
@@ -65,5 +76,5 @@ if __name__ == "__main__":
     word_list = set(words.words())
     df = pd.read_excel('nltk_spellchecking/ELECTION_2024.xlsx')
 
-    file_name = "name_eng_prompts.txt"
-    run_test(file_name, "everything")
+    file_name = "candidate_names.txt"
+    run_test(file_name, "just name", 10)
